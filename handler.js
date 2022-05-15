dc81130709a2b1fe3dd9867f72d51946521e2d54
Togami Yuki <togaminnnn@gmail.com>
2022-05-15 18:31:23 +0900

    メッセージ編集

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'aws-github-codebuild-test 自動デプロイ。',
+        message: 'aws-github-codebuild-test 自動デプロイする。',
         input: event,
       },
       null,
